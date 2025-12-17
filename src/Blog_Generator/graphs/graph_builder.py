c08b211c5b0b9a2036950bb49a2b4cf03729e463
Naveen-DS08 <naveenmails814@gmail.com>
2025-12-18 00:13:45 +0530

    Langsmith studio integrated

--- src/Blog_Generator/graphs/graph_builder.py
+++ src/Blog_Generator/graphs/graph_builder.py
@@ -30,3 +30,9 @@ class GraphBuilder:
             graph = self.build_topic_graph()
 
         return graph.compile()
+    
+# Below code is for langsmith studio
+llm = GroqLLM().get_llm()
+
+graph_builder = GraphBuilder(llm)
+graph = graph_builder.build_topic_graph().compile()
